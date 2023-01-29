2dfea4ef99a5dd592daae5509855213f0e4b47e2
agustin mateos <agustinrotondomateos@gmail.com>
2023-01-29 20:44:05 -0300

    first commit

---
+++ main.js
@@ -0,0 +1,6 @@
+document.addEventListener('mousemove', function (e) {
+        const scratch = document.createElement('span');
+        const body = document.querySelector('body');
+    
+    scratch.style 
+    });
